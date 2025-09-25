d7ef4c1e5d37d3fc8915047ebe6cc2c0a94dbab2
Aryan <mail@ba3a.tech>
2025-09-26 04:45:48 +0530

    chore: bump version to 0.0.6 and update random bytes generation in CA class to remove dependency on crypto

--- src/ca.ts
+++ src/ca.ts
@@ -1,5 +1,6 @@
 import { ed25519 } from "@noble/curves/ed25519.js";
 import { sha256 } from "@noble/hashes/sha2.js";
+import { randomBytes } from "@noble/hashes/utils.js";
 import { Signer } from "./signer";
 import type {
     AadhaarCSRData,
@@ -18,8 +19,7 @@ export class CA {
     }
 
     public generateAadhaarCertificate(csrData: AadhaarCSRData, userPublicKeyBytes: Uint8Array): AadhaarCertificateRaw {
-        const randomBytes = crypto.getRandomValues(new Uint8Array(8));
-        const serial = sha256(new Uint8Array([...Buffer.from(JSON.stringify(csrData)), ...Buffer.from(randomBytes)])).toBase64();
+        const serial = sha256(new Uint8Array([...Buffer.from(JSON.stringify(csrData)), ...Buffer.from(randomBytes(8))])).toBase64();
         const issuer = sha256(new Uint8Array(this.publicKeyBytes)).toBase64();
         const validFrom = String(Date.now());
         const validTo =  String(validFrom + 180 * 24 * 60 * 60 * 1000); // 180 days validity
